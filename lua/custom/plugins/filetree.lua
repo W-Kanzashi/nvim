82ef2518bd7c1c23644a65ac4057c917831449b1
Kanzashi Watanabe <meung@ameung.com>
2024-04-30 09:12:48 +0200

    Update config

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -16,7 +16,7 @@ return {
       enable_git_status = true,
       enable_diagnostics = true,
       window = {
-        width = 25,
+        width = 40,
       },
       filesystem = {
         filtered_items = {
